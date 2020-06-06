c2a35289d594eb7a089fcb79f9eefa1da3925723
lucianobs1 <lucianotetsu@gmail.com>
2020-06-06 19:04:54 -0300

    Adicionado estilizaçao em formulario e logica de adicionar e remover itens

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -23,11 +23,15 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>";
+    citySelect.disabled = true;
+
     fetch(url)
         .then(res => res.json())
         .then(cities => {
+
             for (city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false;
@@ -38,4 +42,40 @@ function getCities(event) {
 
 document
     .querySelector('select[name=uf]')
-    .addEventListener('change', getCities)
+    .addEventListener('change', getCities);
+
+
+// Logica para adicionar e remover itens no array 
+const itemsCollect = document.querySelectorAll('.items-grid li');
+
+for (let item of itemsCollect) {
+    item.addEventListener('click', handleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+    const itemLi = event.target;
+
+    itemLi.classList.toggle("selected");
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex(function(item) {
+        const itemFound = item == itemId;
+        return itemFound;
+    });
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+        })
+
+        selectedItems = filteredItems;
+    } else {
+        selectedItems.push(itemId);
+    }
+    collectedItems.value = selectedItems;
+}
